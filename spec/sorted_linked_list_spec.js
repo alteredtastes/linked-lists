0984119dc7355b12e1bcb005d64cd7694780fda5
Tim Garcia <tim.m.garcia@gmail.com>
2015-12-07 23:24:42 -0800

    Adding stub for sorted linked list

---
+++ spec/sorted_linked_list_spec.js
@@ -0,0 +1,9 @@
+var SortedLinkedList = require("../sorted_linked_list");
+var list;
+
+describe("SortedLinkedList", function() {
+  beforeEach(function() {
+    list = new SortedLinkedList();
+  });
+
+});
